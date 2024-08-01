5da94c6b9b8cfeb2239632ba110cda3820ddc36f
Cristian Lee <cristianalonsolee@gmail.com>
2024-08-01 13:51:50 -0600

    first commit

---
+++ src/main/java/com/task/ms/task/entity/Task.java
@@ -0,0 +1,35 @@
+package com.task.ms.task.entity;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+@Table(name = "t_task")
+public class Task implements Serializable{
+
+	@Serial
+	private static final long serialVersionUID = -7853544041236457000L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String title;
+	
+	
+	
+}
